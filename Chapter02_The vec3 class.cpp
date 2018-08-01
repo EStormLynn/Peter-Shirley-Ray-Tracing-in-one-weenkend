d546197c772cf039f553925a225cfd9ddd8a6059
EStormLynn <zhang3158627@sina.com>
2018-08-02 00:07:34 +0800

    Complete vec3 class

--- Chapter02_The vec3 class.cpp
+++ Chapter02_The vec3 class.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "vec3.h"
+
 using namespace std;
 
 
@@ -12,12 +12,13 @@ int main()
     {
         for(int i=0;i<nx;i++)
         {
-            float(5.0/5);
-            vec3 col((float(i)/float(nx)),(float(j)/float(ny)),0.2);
+            float r=float(i)/float(nx);
+            float g=float(j)/float(ny);
+            float b=0.2;
 
-            int ir=int(255.99* col[0]);
-            int ig=int(255.99* col[1]);
-            int ib=int(255.99* col[2]);;
+            int ir=int(255.99*r);
+            int ig=int(255.99*g);
+            int ib=int(255.99*b);
             cout<<ir<<" "<<ig<<" "<<ib<<"\n";
         }
     }
